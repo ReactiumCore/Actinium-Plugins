7ba208d8e3ed8ea2e3b5c1accd5bf5ccf134dc68
Cam Tullos <ctullos@illumifi.net>
2024-03-11 12:23:40 -1000

    WIP: Content (#11)
    Updated actinium-core upload max size crap.
    
    Co-authored-by: Cam Tullos <cam@reactium.io>

--- actinium_modules/@atomic-reactor/actinium-core/middleware/body_parser/middleware.js
+++ actinium_modules/@atomic-reactor/actinium-core/middleware/body_parser/middleware.js
@@ -1,10 +1,10 @@
-import bodyParser from 'body-parser';
+import express from 'express';
 
 Actinium.Middleware.register(
     'body-parser',
     (app) => {
-        app.use(bodyParser.json({ limit: '50mb' }));
-        app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+        app.use(express.json({ limit: ENV.MAX_UPLOAD_SIZE, }));
+        app.use(express.urlencoded({ extended: true, limit: ENV.MAX_UPLOAD_SIZE }));
         return Promise.resolve();
     },
     -100000,
